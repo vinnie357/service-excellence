80dfd5a605a68ca5650ac8e18f4dc8687cb7e318
vinnie357 <vinnie.mazza@gmail.com>
2021-06-04 21:11:30 +0000

    waypoint autocomplete

--- .devcontainer/scripts/waypoint-cli-debian.sh
+++ .devcontainer/scripts/waypoint-cli-debian.sh
@@ -12,5 +12,5 @@ apt-get install software-properties-common -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install waypoint=${waypointVersion} -y
-
+waypoint -autocomplete-install
 echo "---waypoint-cli done---"
